90ad03fe55d772a357436a6a8dae67d0fb966148
selmasaltik <selmasaltiik@gmail.com>
2024-12-04 05:54:34 +0300

    Updating react-router-dom latest version

--- 29-animations/src/App.jsx
+++ 29-animations/src/App.jsx
@@ -6,6 +6,16 @@ import ChallengesPage from './pages/Challenges.jsx';
 const router = createBrowserRouter([
   { path: '/', element: <WelcomePage /> },
   { path: '/challenges', element: <ChallengesPage /> },
+  {
+    future: {
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_skipActionErrorRevalidation: true,
+      v7_startTransition: true,
+    }
+  }
 ]);
 
 function App() {
